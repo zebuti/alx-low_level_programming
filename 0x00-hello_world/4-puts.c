d2a4d8a452f4025e9e02810ad5db6dadef49d024
zebuti <zebuti@example.com>
2022-09-08 15:05:28 +0300

    1232

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * return: 0
+ */
+int main(void)
+{
+	puts("\'programming is like building a multilingual puzzle");
+	return (0)
+}
