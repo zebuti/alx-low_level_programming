19e9b1fdfaffae97932ab2a5fc0f397a185aadac
zebuti <zebuti@example.com>
2022-09-08 15:29:19 +0300

    efr

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - print the string in the put function
+ *  Description: using the main function
+ *  this program prints "programing is like building a multilingual puzzle
+ *  Return: 0
+ */
+
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf ("size of a chr: %ld byte(s)\n", sizeof(c));
+	printf ("size of an int: %ld byte(s)\n", sizeof(i));
+	return (0);
+}
