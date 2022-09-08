4eac38205eec5205bef0571cc8f5758b26c6f049
zebuti <zebuti@example.com>
2022-09-08 15:18:25 +0300

    3423

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * main - print the string to the put function
+ *
+ *  Description: using the main function
+ *  this program prints -programing is like building a multilingual puzzle
+ *  return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a price of art,\n");    
+	return (0);
+}
