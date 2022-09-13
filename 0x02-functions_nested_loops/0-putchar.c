b1b08d0f7ff06787f8fe31ee13c59613b1f515c1
zebuti <102354890+zebuti@users.noreply.github.com>
2022-09-13 15:30:50 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 /**
- * main - Entry point
+ * main -prints  putchar
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
-{
-	char *sh = "Holberton";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
+{	
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
 
 	return (0);
 }
