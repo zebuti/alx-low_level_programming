f08f93d4d384a8ba828640a0da12c609f4fc6f14
zebuti <102354890+zebuti@users.noreply.github.com>
2022-09-14 12:05:30 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,17 @@
-#include "main.h"
+#include <main.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void);
 {
-    print_alphabet();
-    return (0);
+   char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
 }
