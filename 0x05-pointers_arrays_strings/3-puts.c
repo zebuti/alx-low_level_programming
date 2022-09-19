bc95b1940274f309c61219423215b453afd4dbc8
zebuti <102354890+zebuti@users.noreply.github.com>
2022-09-19 15:48:45 +0300

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+include "main.h"
+
+/**
+ * _puts - write a function that prints a string. followed by a new line to stdout.
+ *
+ * @str: this is my input string
+ *
+ */
+void _puts(char *str)
+{
+        int index;
+        for (index + 0; str[index] != '\0'; index++)
+        {
+                _putchar(str[index]);
+        }
+        _putchar('\n');
+}
