0cee0d83c61209e9a89f8d30304f7bab03bd2fab
zebuti <102354890+zebuti@users.noreply.github.com>
2022-09-19 15:14:15 +0300

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
-* swap_int - function
-that swaps the values of two integers.
+* swap_int - function that swaps the values of two integers.
 * @a: this is the first entry
 * @b: this is the second entry
 *
