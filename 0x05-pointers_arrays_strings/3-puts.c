586ca543dfa32abbad294d6cb5d83eb13823a077
zebuti <102354890+zebuti@users.noreply.github.com>
2022-09-19 15:50:49 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,7 +9,7 @@
 void _puts(char *str)
 {
         int index;
-        for (index + 0; str[index] != '\0'; index++)
+        for (index = 0; str[index] != '\0'; index++)
         {
                 _putchar(str[index]);
         }
