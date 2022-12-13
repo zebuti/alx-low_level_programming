441cab26e2069975cbd565799c277dc600179853
zebuti <102354890+zebuti@users.noreply.github.com>
2022-12-13 16:26:48 +0300

    Update main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,45 @@
+#ifndef main_h
+#define HOLBERTON_H
+
+#include <unistd.h>
+
+/* 0 */
+int _putchar(char c);
+
+/* 1 */
+void print_alphabet(void);
+
+/* 2 */
+void print_alphabet_x10(void);
+
+/* 3 */
+int _islower(int c);
+
+/* 4 */
+int _isalpha(int c);
+
+/* 5 */
+int print_sign(int n);
+
+/* 6 */
+int _abs(int);
+
+/* 7 */
+int print_last_digit(int);
+
+/* 8 */
+void jack_bauer(void);
+
+/* 9 */
+void times_table(void);
+
+/* 10 */
+int add(int, int);
+
+/* 11 */
+void print_to_98(int n);
+
+/* 12 */
+void print_times_table(int n);
+
+#endif /* main_h */
