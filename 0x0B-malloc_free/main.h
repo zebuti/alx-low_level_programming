727be0197d3784d494d80c0d477178b95de2b221
zebuti <102354890+zebuti@users.noreply.github.com>
2023-01-10 09:33:39 +0300

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 
@@ -13,4 +13,4 @@ int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
-#endi
+#endif
