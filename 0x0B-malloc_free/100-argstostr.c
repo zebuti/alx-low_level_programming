4c1ec494042c5122fcef3ece87551a4294800f06
zebuti <102354890+zebuti@users.noreply.github.com>
2023-01-10 09:58:25 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,79 @@
-#include <stdlib.h>
 #include "main.h"
-
+		
+#include <stdlib.h>
+		
 /**
-* *argstostr - concatenates all the arguments of the program
-* @ac: number of arguments
-* @av: array of arguments
-*
-* Return: Pointer to the new string (Success), NULL (Error)
-*/
+		
+ * argstostr - main entry
+		
+ * @ac: int input
+		
+ * @av: double pointer array
+		
+ * Return: 0
+		
+ */
+		
 char *argstostr(int ac, char **av)
+		
 {
-	int i, j, k, len;
+		
+	int i, n, r = 0, l = 0;
+		
 	char *str;
+		
 
+		
 	if (ac == 0 || av == NULL)
+		
 		return (NULL);
+		
 
+		
 	for (i = 0; i < ac; i++)
+		
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
+		
+		for (n = 0; av[i][n]; n++)
+		
+			l++;
+		
 	}
+		
+	l += ac;
+		
 
-	str = malloc(sizeof(char) * (len + 1));
-
+		
+	str = malloc(sizeof(char) * l + 1);
+		
 	if (str == NULL)
+		
 		return (NULL);
-
-	k = 0;
-
+		
 	for (i = 0; i < ac; i++)
+		
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
+		
+	for (n = 0; av[i][n]; n++)
+		
+	{
+		
+		str[r] = av[i][n];
+		
+		r++;
+		
 	}
-	
+		
+	if (str[r] == '\0')
+		
+	{
+		
+		str[r++] = '\n';
+		
+	}
+		
+	}
+		
 	return (str);
+		
 }
