a6c9128e53d823819c60bcca9cf93dca2c0e1f49
zebuti <102354890+zebuti@users.noreply.github.com>
2023-01-10 10:13:59 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,8 +10,8 @@ char *argstostr(int ac, char **av)
 {
 	int i, n, r = 0, l = 0;
 	char *str;
-	if (ac == 0 || av == NULL)
 	
+	if (ac == 0 || av == NULL)
 	return (NULL);
 	for (i = 0; i < ac; i++)
 		
