567602c2e071fd2f0361476f535c8dc75b956373
zebuti <102354890+zebuti@users.noreply.github.com>
2023-01-11 09:54:11 +0300

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -9,33 +9,3 @@ int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
- 
- 
- 
- 
- 
-4-strpbrk.c
-		
-#include "main.h"
-/**
- * _strpbrk - Entry point
- * @s: input
- * @accept: input
- * Return: Always 0 (Success)
- */
-char *_strpbrk(char *s, char *accept)
-{
-	int k;
- 
-	while (*s)
-	{
-		for (k = 0; accept[k]; k++)
-		{
-		if (*s == accept[k])
-		return (s);
-		}
-	s++;
-	}
- 
-return ('\0');
-}
