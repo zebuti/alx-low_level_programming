7aed03c359916a839014cda8911568623edd80cc
zebuti <102354890+zebuti@users.noreply.github.com>
2023-01-16 14:51:19 +0300

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,12 +3,12 @@
 	#include "dog.h"
 
 
-	/**
+/**
 	 * print_dog - prints a struct dog
 	 * @d: struct dog to print
-	 */
+**/
 	void print_dog(struct dog *d)
-	{
+{
 		if (d == NULL)
 			return;
 
@@ -20,4 +20,4 @@
 
 
 		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-	}
+}
