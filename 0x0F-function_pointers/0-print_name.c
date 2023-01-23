6693623b018f1286d3b9eab52081d626bed13c28
zebuti <102354890+zebuti@users.noreply.github.com>
2023-01-23 11:32:46 +0300

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *));
+{
+    if (name && f)
+      f( name):
+}
