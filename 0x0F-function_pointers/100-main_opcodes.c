0cdcaa8a32b3c6cc427dcb9cdd80024f9dd44daa
zebuti <102354890+zebuti@users.noreply.github.com>
2023-01-23 11:53:42 +0300

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -3,11 +3,10 @@
 
 
 /**
-	 * main - prints its own opcodes
-	 * @argc: number of arguments
-	 * @argv: array of arguments
-	 *
-	 * Return: Always 0 (Success)
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
 **/
 int main(int argc, char *argv[])
 {
@@ -16,19 +15,18 @@ int main(int argc, char *argv[])
 
 
 		if (argc != 2)
-    {
-			printf("Error\n");
-			exit(1);
-		}
+ {
+		printf("Error\n");
+		exit(1);
+}
 
 
-		bytes = atoi(argv[1]);
+bytes = atoi(argv[1]);
 
-
-		if (bytes < 0)
-		{
-			printf("Error\n");
-			exit(2);
+ if (bytes < 0)
+{
+      printf("Error\n");
+    exit(2);
 		}
 
 
