838bde1aa80b7344e257bcea47a7ff83a7757e4c
zebuti <102354890+zebuti@users.noreply.github.com>
2023-01-30 11:57:35 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_list/1-listint_len.c
+++ 0x13-more_singly_linked_list/1-listint_len.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
  * listint_len -  returns the number of elements in a linked list
- * 
+ * @h: linked list of type listint_t to traverse
+ 
  * Return: the number of nodes
  **/
 size_t listint_len(const listint_t *h);
