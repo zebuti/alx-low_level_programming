1058f860fcb8850a94f5881a660a4f011671cbee
zebuti <102354890+zebuti@users.noreply.github.com>
2023-02-06 14:15:18 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,25 +9,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
-	char *buf;
+ int fd;
+ ssize_t nrd, nwr;
+ char *buf;
 
-	if (!filename)
+if (!filename)
 		return (0);
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+   fd = open(filename, O_RDONLY);
+if (fd == -1)
 		return (0);
-
-	buf = malloc(sizeof(char) * (letters));
+buf = malloc(sizeof(char) * (letters));
 	if (!buf)
 		return (0);
-
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
-
+nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd); 
 	close(fd);
 
 	free(buf);
