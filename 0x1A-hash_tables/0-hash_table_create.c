655d3bcd6d42cb50098b94ee1eb4c257e2eb3193
zebuti <102354890+zebuti@users.noreply.github.com>
2023-04-07 16:31:07 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,66 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * free_node - Free a node.
- * @node: Node to free.
+ * hash_table_create - creates a hash table
+ * @size: size of the array
  *
- * Return: Void.
+ * Return: pointer to the newly created hash table
  */
-void free_node(hash_node_t *node)
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	free(node->key);
-	free(node->value);
-	free(node);
-}
+	hash_table_t *hash_table;
+	unsigned long int i;
 
-/**
- * hash_table_set - Set a value in the hash table.
- * @ht: Hash table.
- * @key: Key to be indexed.
- * @value: Value to set in the hash table.
- *
- * Return: 1 if works, 0 if doesn't.
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int index;
-	hash_node_t *new_node, *current;
-
-	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup((char *)key);
-	new_node->value = strdup((char *)value);
-	new_node->next = NULL;
-	if (ht->array[index] == NULL)
-		ht->array[index] = new_node;
-	else
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		current = ht->array[index];
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next;
-			ht->array[index] = new_node;
-			free_node(current);
-			return (1);
-		}
-		while (current->next != NULL && strcmp(current->next->key, key) != 0)
-		{ current = current->next;
-		}
-		if (strcmp(current->key, key) == 0)
-		{
-			new_node->next = current->next->next;
-			free_node(current->next);
-			current->next = new_node;
-		}
-		else
-		{
-			new_node->next = ht->array[index];
-			ht->array[index] = new_node;
-		}
+		free(hash_table);
+		return (NULL);
 	}
-	return (1);
+	for (i = 0; i < size; i++)
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
